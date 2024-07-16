f94b04650af4b1c04cfe29bcdc680260d4d50a91
Douxx <136928935+douxxu@users.noreply.github.com>
2024-07-16 17:12:04 +0200

    Create updater.service.js

---
+++ src/services/updater.service.js
@@ -0,0 +1,81 @@
+const { exec } = require('child_process');
+const fs = require('fs-extra');
+const path = require('path');
+const packageManagers = require('./package-managers');
+const c = require('colors');
+
+const serviceFilePath = '/etc/systemd/system/deb-autoupdater.service'; 
+
+function enableService() {
+
+    const serviceContent = `
+    [Unit]
+    Description=Linux Auto Updater - https://github.com/douxxu/Lin-autoupdater
+
+    [Service]
+    ExecStart=node ${process.cwd()}/src/index.js update
+    Restart=always
+
+    [Install]
+    WantedBy=multi-user.target
+    `;
+
+    fs.writeFileSync(serviceFilePath, serviceContent);
+
+    exec('systemctl daemon-reload', (err) => {
+        if (err) {
+            console.error('[ ', c.red('ERR'), ' ]', c.grey(`Error reloading systemd: ${err}`));
+        } else {
+            console.log('[ ', c.green('OK'), ' ]', c.grey('Systemd daemon reloaded.'));
+            exec('systemctl enable deb-autoupdater', (err) => {
+                if (err) {
+                    console.error('[ ', c.red('ERR'), ' ]', c.grey(`Error enabling service: ${err}`));
+                } else {
+                    console.log('[ ', c.green('OK'), ' ]', c.grey('Auto-update service enabled.'));
+                }
+            });
+        }
+    });
+}
+
+function disableService() {
+
+    exec('systemctl disable deb-autoupdater', (err) => {
+        if (err) {
+            console.error('[ ', c.red('ERR'), ' ]', c.grey(`Error disabling service: ${err}`));
+        } else {
+            fs.removeSync(serviceFilePath);
+            console.log('[ ', c.green('OK'), ' ]', c.grey('Auto-update service disabled.)'));
+        }
+    });
+}
+
+function updatePackages() {
+    for (const [manager, instance] of Object.entries(packageManagers)) {
+        if (instance) {
+            console.log('[ ', c.yellow('INFO'), ' ]', c.grey(`Updating packages for ${manager}...`));
+            instance.update();
+        }
+    }
+}
+
+function showInfoOnStart() {
+
+
+console.log('[ ', c.yellow('INFO'), ' ]', c.grey('|============================================|'));
+console.log('[ ', c.yellow('INFO'), ' ]', c.grey('|                                            |'));
+console.log('[ ', c.yellow('INFO'), ' ]', c.grey('|Lin-Autoupdater                             |'));
+console.log('[ ', c.yellow('INFO'), ' ]', c.grey('|Github: github.com/douxxu/Lin-Autoupdater   |'));
+console.log('[ ', c.yellow('INFO'), ' ]', c.grey('|Npmjs: npmjs.org/package/Lin-Autoupdater    |'));
+console.log('[ ', c.yellow('INFO'), ' ]', c.grey('|Project is under a MIT license              |'));
+console.log('[ ', c.yellow('INFO'), ' ]', c.grey('|                                            |'));
+console.log('[ ', c.yellow('INFO'), ' ]', c.grey('|============================================|'));
+
+}
+
+module.exports = {
+    enableService,
+    disableService,
+    updatePackages,
+    showInfoOnStart
+};
