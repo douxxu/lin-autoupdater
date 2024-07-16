407294e315da7afa612c41054bf871675af1173e
Douxx <136928935+douxxu@users.noreply.github.com>
2024-07-16 17:10:49 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const { enableService, disableService, updatePackages, showInfoOnStart } = require('./services/updater.service');
+const c = require('colors');
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+    showInfoOnStart();
+    console.log('[ ', c.red('ERR'), ' ]', c.grey('No command provided. Use enable, disable, notify, or update.'));
+    process.exit(1);
+}
+
+const command = args[0];
+
+switch (command) {
+    case 'enable':
+        showInfoOnStart();
+        enableService();
+        break;
+    case 'disable':
+        showInfoOnStart();
+        disableService();
+        break;
+    case 'update':
+        showInfoOnStart();
+        updatePackages();
+        break;
+    default:
+        console.log('[ ', c.red('ERR'), ' ]', c.grey('Unknown command. Use enable, disable, notify, or update.'));
+        break;
+}
